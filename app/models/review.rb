99279656a00b362110f31101b2d04d13cada4cba
Jonathan Bergerot <jobergerot@gmail.com>
2023-02-22 11:38:45 +0100

    add model validations and pass the test

---
+++ app/models/review.rb
@@ -0,0 +1,9 @@
+class Review < ApplicationRecord
+  RATING = (0..5)
+  belongs_to :restaurant
+
+  validates :content, presence: true
+  validates :rating, presence: true
+  validates :rating, inclusion: { in: RATING }
+  validates :rating, numericality: { only_integer: true }
+end
